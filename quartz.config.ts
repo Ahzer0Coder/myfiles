b9143af33f233f2cea5f53460f287853099bf0d8
Ahzer0Coder <ahmednagj100@gmail.com>
2026-05-13 21:20:59 +0300

    Update baseUrl in quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "ahzer0coder.github.io/myfiles"
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {

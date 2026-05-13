6954ffff17428950cb28e35cc1a2ec36b3a63dfc
Ahzer0Coder <ahmednagj100@gmail.com>
2026-05-13 21:25:15 +0300

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "ahzer0coder.github.io/myfiles"
+    baseUrl: "ahzer0coder.github.io/myfiles",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
